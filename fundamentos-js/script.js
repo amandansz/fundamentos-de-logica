7df2623313f7af8041d2e82b1ea06a91e065162c
Amanda Nunes de Souza <dev.amandasouza@gmail.com>
2026-01-28 18:55:48 -0300

    first commit

---
+++ fundamentos-js/script.js
@@ -0,0 +1,11 @@
+console.log("Bem-vindo à cozinha da Maria!")
+let bolo = "bolo de chocolate"
+let cafe = "café 3 corações"
+let biscoito = "biscoito de polvilho"
+
+const mensagemDaMaria = "Na minha cozinha tem: " +
+bolo + ", " + 
+cafe + " e " +
+biscoito + "."
+
+console.log(mensagemDaMaria)
