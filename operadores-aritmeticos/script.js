b3d04021806c1c589aafdf6c9204662692fe083e
Amanda Nunes de Souza <dev.amandasouza@gmail.com>
2026-01-30 23:10:53 -0300

    First commit

---
+++ operadores-aritmeticos/script.js
@@ -0,0 +1,9 @@
+let multiplicador = 4
+let multiplicando = 8
+let produto = multiplicador * multiplicando
+console.log( "O resultado da multiplicação é: " + produto)
+
+for (let i = 1; i < 10; i++) {
+    let resultado = i * 7
+    console.log(resultado)
+}
