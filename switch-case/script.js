617a6025288c9ff8bb2010c46e20b186b7f818e7
Amanda Nunes de Souza <dev.amandasouza@gmail.com>
2026-02-05 18:58:52 -0300

    feat: adiciona exercício com switch case

---
+++ switch-case/script.js
@@ -0,0 +1,18 @@
+let opcao = 1;
+
+switch (opcao) {
+    case 1:
+        console.log("Você escolheu a opção 1");
+        break;
+
+    case 2:
+        console.log("Você escolheu a opção 2");
+        break;
+
+    case 3:
+        console.log("Você escolheu a opção 3");
+        break;
+
+    default:
+        console.log("Opção inválida");
+}
