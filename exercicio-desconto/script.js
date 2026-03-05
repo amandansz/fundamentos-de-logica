14a80453b4be4306ab51cdf7d8472bb1bbbc7f8a
Amanda Nunes de Souza <dev.amandasouza@gmail.com>
2026-03-04 22:52:23 -0300

    feat: adiciona exercício de desconto

---
+++ exercicio-desconto/script.js
@@ -0,0 +1,13 @@
+let valorCompra = 120;
+let formaPagamento = "avista";
+let compra;
+
+
+if (valorCompra >= 100 && formaPagamento === "avista") {
+    console.log("Parabéns! Você ganhou um desconto de 10% na sua compra de R$ " + valorCompra.toFixed(2));
+    compra = valorCompra * 0.10;
+    console.log("O valor do desconto é: R$ " + compra.toFixed(2) + "\nTotal a pagar: R$ " + (valorCompra - compra).toFixed(2));
+}
+else {
+    console.log("Desculpe, você não se qualifica para o desconto.");
+}
