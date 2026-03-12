76eaf68bf500be893b8d0e3b233ba284b0f5c808
Amanda Nunes de Souza <dev.amandasouza@gmail.com>
2026-03-12 20:44:11 -0300

    feat: reformula script adicionando objeto e funções

--- exercicio-desconto/script.js
+++ exercicio-desconto/script.js
@@ -1,13 +1,36 @@
-let valorCompra = 120;
-let formaPagamento = "avista";
-let compra;
+function calcularDesconto(valorCompra, formaPagamento) {
 
+    const pagamento = {
+        avista: "avista",
+        debito: "debito",
+        credito: "credito"
+    };
 
-if (valorCompra >= 100 && formaPagamento === "avista") {
-    console.log(`Parabéns! Você ganhou um desconto de 10% na sua compra de R$ ${valorCompra.toFixed(2)}`);
-    compra = valorCompra * 0.10;
-    console.log(`O valor do desconto é: R$ ${compra.toFixed(2)}\nTotal a pagar: R$ ${(valorCompra - compra).toFixed(2)}`);
+    let desconto = 0;
+
+    if (valorCompra >= 300 && formaPagamento === pagamento.avista) {
+        desconto = valorCompra * 0.15;
+    } 
+    else if (valorCompra >= 200 && formaPagamento === pagamento.debito) {
+        desconto = valorCompra * 0.05;
+    } 
+    else {
+        console.log("Desculpe, você não se qualifica para o desconto.");
+    }
+
+    let total = valorCompra - desconto;
+
+    return {
+        valorCompra,
+        desconto,
+        total
+    };
 }
-else {
-    console.log("Desculpe, você não se qualifica para o desconto.");
-}
+
+let resultado = calcularDesconto(350, "avista");
+console.log(`----------------------------------------`);
+console.log(`O valor da sua compra é: R$ ${resultado.valorCompra.toFixed(2)}`);
+console.log(`----------------------------------------`);
+console.log(`Desconto aplicado: R$ ${resultado.desconto.toFixed(2)}`);
+console.log(`----------------------------------------`);
+console.log(`Total a pagar: R$ ${resultado.total.toFixed(2)}`);
